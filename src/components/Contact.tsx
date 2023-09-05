86a27a24bc0c45dc6639eddcdb29284eae38a5ab
scottlee <scottlee0191@gmail.com>
2023-09-05 21:15:59 +0700

    add fake submit

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,13 +5,16 @@ function Contact() {
   function handleProfile() {
     toProfile()
   }
+  const handleSubmit = () => {
+    alert('Sent!')
+  }
   return (
     <div>
       <h2>Contact Me!</h2>
-      <form className="mt-6 space-y-6">
-        <input placeholder="Full Name" />
-        <input placeholder="Email Address" type="email" />
-        <textarea placeholder="Your Message" rows={8} />
+      <form className="mt-6 space-y-6" onSubmit={handleSubmit}>
+        <input placeholder="Full Name" required />
+        <input placeholder="Email Address" type="email" required />
+        <textarea placeholder="Your Message" rows={8} required />
         <button className="mx-auto block border-2 border-main bg-main text-white hover:bg-transparent hover:text-main">
           Send Message
         </button>
